d9debdecd12ba812d133785a5c857c4314953095
Robson Torres Costa <robsoncosta1234567@gmail.com>
2024-03-22 16:37:57 -0300

    Adicionada a funcao de inserir frutas

--- OneDrive/Documentos/Kenzie/projetosKenzie/Back End/M5/Entrega 3 - Desenvolvendo e testando uma API de catálogo de carros/src/interfaces/car.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Car } from "@prisma/client";
-import { z } from "zod";
-import { carReturnSchema } from "../schemas/CarSchema";
-
-type CarCreate = Omit<Car, "id">;
-type CarUpdate = Omit<Car, "id">;
-type CarReturn = z.infer<typeof carReturnSchema>;
-
-export { CarCreate, CarReturn, CarUpdate };
